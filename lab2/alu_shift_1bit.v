4cfc5cd570b6370532e093a45073d0300b62fdd3
Ivan <ivan.sinyagin@me.com>
2014-09-29 19:59:13 -0400

    1 bit shifter improved

--- lab2/alu_shift_1bit.v
+++ lab2/alu_shift_1bit.v
@@ -8,13 +8,16 @@
 //////////////////////////////////////////////////////////////////////////////////
 module alu_shift_1bit(A, S, Z);
 
-    parameter N = 1;
+    parameter N = 2;
 
 	//port definitions
-	input wire A, S, Z;
-    wire B;
 
-    assign B = ~A;
+	input wire [(N-1):0] A, S;
+    output [(N-1):0] Z;
+    wire [(N-1):0] B;
+
+    assign B[(N-2):0] = A[N:1];
+    assign B[(N-1):(N-2)] = 1'b0;
     
 	mux_2to1 #(.N(N)) MUX (.X(A), .Y(B), .S(S), .Z(Z));
 endmodule
