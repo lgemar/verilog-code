0c1f791bf8ee9df8586ae6de514ea97b167abdf8
Ivan Sinyagin <sinyagin@post.harvard.edu>
2014-10-01 10:09:01 -0400

    sra modules for testing

--- lab2/alu_sra.v
+++ lab2/alu_sra.v
@@ -8,15 +8,20 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module alu_sra(A, Z);
-	parameter WIDTH = 32;
-	parameter SHIFT = 0;
- 
-	input wire [(WIDTH - 1):0] A;
-	output wire [(WIDTH - 1):0] Z;
-	
-	reg [31:0] TEMP = 32'hffffffff;
+    	parameter N = 32;
 
-	assign Z[(WIDTH-1):(WIDTH-1)-SHIFT] = TEMP[(WIDTH-1):(WIDTH-1)-SHIFT];
+	//port definitions
 
-	assign Z[31-SHIFT:0] = A[31:SHIFT];
+	input wire [(N-1):0] A;
+	input wire [5:0]S;
+    	output wire [(N-1):0] Z;
+    	wire [(N-1):0] shifted2, shifted4, shifted8, shifted16, shifted32;
+
+	alu_sra_32bit #(.N(N)) MUX6 (.A(A), .S(S[5]), .Z(shifted32));
+	alu_sra_16bit #(.N(N)) MUX5 (.A(shifted32), .S(S[4]), .Z(shifted16));
+	alu_sra_8bit #(.N(N)) MUX4 (.A(shifted16), .S(S[3]), .Z(shifted8));
+	alu_sra_4bit #(.N(N)) MUX3 (.A(shifted8), .S(S[2]), .Z(shifted4));
+
+	alu_sra_2bit #(.N(N)) MUX2 (.A(shifted4), .S(S[1]), .Z(shifted2));
+	alu_sra_1bit #(.N(N)) MUX1 (.A(shifted2), .S(S[0]), .Z(Z));
 endmodule
---
+++ lab2/alu_sra_2bit.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+`default_nettype none
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Module Name:    alu_shift_2bit
+// Description: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu_sra_2bit(A, S, Z);
+
+    parameter N = 2;
+
+	//port definitions
+
+	input wire [(N-1):0] A;
+	input wire S;
+    output wire [(N-1):0] Z;
+    wire [(N-1):0] B;
+
+    assign B[N-3:0] = A[N-1:2];
+    assign B[N-1:N-2] = 2'b1;
+    
+	mux_2to1 #(.N(N)) MUX (.X(A), .Y(B), .S(S), .Z(Z));
+endmodule
+`default_nettype wire
