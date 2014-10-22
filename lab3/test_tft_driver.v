f5bf1108b60761b253023bffd97adf4f9651500d
Ivan Sinyagin <sinyagin@post.harvard.edu>
2014-10-22 15:54:14 -0400

    necessary register

--- lab3/test_tft_driver.v
+++ lab3/test_tft_driver.v
@@ -2,38 +2,19 @@
 
 `default_nettype none //makes undeclared nets errors instead of automatic wires
 
-module main(
-	//default IO
-	input wire unbuf_clk, rstb,
-	input wire [7:0] switch,
-	output wire [7:0] led,
-	output wire [7:0] JB,
-	input wire button_up, button_down, button_right, button_left, button_center,
-	//tft IO
-	output wire tft_backlight, tft_clk, tft_data_ena,
-	output wire tft_display,tft_vdd,
-	output wire [7:0] tft_red, tft_green, tft_blue
-);
+module test_tft_driver;
+
+reg [7:0] tft_red, tft_green, tft_blue
+reg tft_backlight, tft_clk, tft_data_ena,
+reg tft_display,tft_vdd,
 
 //clocking signals
-wire cclk, cclk_n, tft_clk_buf, tft_clk_buf_n, clocks_locked;
-wire reset;
-assign reset = ~rstb;
+reg cclk, cclk_n, tft_clk_buf, tft_clk_buf_n, clocks_locked;
 
 //tft signals
-wire [9:0] tft_x;
-wire [8:0] tft_y;
-wire tft_new_frame;
-
-//debugging signals
-//feel free to connect signals here so that you can probe/see them
-assign led = 0; 
-assign JB = 8'b0; 
-
-//generate all the clocks
-clock_generator CLOCK_GEN (.clk_100M_in(unbuf_clk), .CLK_100M(cclk), .CLK_100M_n(cclk_n), .CLK_9M(tft_clk_buf), .CLK_9M_n(tft_clk_buf_n), .RESET(reset), .LOCKED(clocks_locked));
-//pass the tft_clk through a DDR2 output buffer (so that it can drive external loads and so that internal loads are unaffected by large skew routing)
-ODDR2 tft_clk_fixer (.D0(1'b1), .D1(1'b0), .C0(tft_clk_buf), .C1(tft_clk_buf_n), .Q(tft_clk), .CE(1'b1));
+reg [9:0] tft_x;
+reg [8:0] tft_y;
+reg tft_new_frame;
 
 tft_driver TFT(
 	.cclk(cclk),
