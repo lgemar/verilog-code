33b13b23b7eccca970d8dab673d9cd4ed31a3734
Ivan Sinyagin <sinyagin@post.harvard.edu>
2014-10-28 14:21:26 -0400

    initial test for test_touch_controller

--- lab3/test_touch_controller.v
+++ lab3/test_touch_controller.v
@@ -4,62 +4,38 @@
 
 module test_touch_controller;
 
-// Color outputs
-wire [7:0] tft_red, tft_green, tft_blue;
-// Color flags
-reg [1:0] color;
-// Clocking inputs
-reg cclk, rstb, tft_clk;
-// Clocking counters
-reg [31:0] px_count;
-// Screen / Backlight variables
-wire tft_backlight, tft_data_ena;
-// Unused input variable for backlight
-reg [7:0] switch;
-// Boring variables that are 0 during reset and 1 otherwise
-wire tft_display,tft_vdd;
-// register counters and enables
-reg [31:0] iter; 
-reg prev_enable;
-// iterator 
+    reg cclk, rstb, touch_clk, touch_busy, touch_data_in, touch_data_out, touch_csb;
+    //touchpad signals
+    wire [11:0] touch_x, touch_y, touch_z;
 
-//tft signals
-wire [9:0] tft_x;
-wire [8:0] tft_y;
-wire tft_new_frame;
+    wire reset;
+    assign reset = ~rstb;
 
-touchpad_controller TFT(
-);
+    touchpad_controller TFT(
+        .cclk(cclk), 
+        .rstb(rstb), 
+        .touch_clk(touch_clk),
+        .touch_busy(touch_busy),
+        .data_in(touch_data_out),
+        .data_out(touch_data_in),
+        .touch_csb(touch_csb),
+        .x(touch_x),
+        .y(touch_y),
+        .z(touch_z)
+    );
+
+
 	initial begin 
-		// Insert the dumps here
-		$dumpfile("test_tft_driver.vcd");
-		$dumpvars(0, test_tft_driver);
+        // Insert the dumps here
+        $dumpfile("test_touch_controller.vcd");
+        $dumpvars(0, test_touch_controller);
+
+        @always #5 cclk = ~cclk;
+        @always touch_data_out = $random();
+
+        
 		// Initialize Inputs
-		cclk = 0;
-		switch = 123;
-		// Color flags
-		color = 0;
-		// Clocking inputs
-		rstb = 1; 
-		tft_clk = 0;
-		// Clocking counters
-		px_count = 0;
-		// Unused input variable for backlight
-		// reg 
-		iter = 0;
-		// Wait 100 ns for global reset to finish
-		#100;
-		// Reset the device
-		rstb = 0;
-		#100;
-		rstb = 1;
-		// Set the previous enable to 1
-		prev_enable = 1;
-		// Set loop counters to 0
-		iter = 0;
-		// Wait for global reset
-		#100;
-		// Reset modules to put them in a known state
+        
 		$finish;
 	end
 endmodule
