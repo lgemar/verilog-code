0a08ab72a9002ce0ec8dc971e4673c00fcc89040
Lukas Gemar <lukasgemar@gmail.com>
2014-11-01 18:48:49 -0400

    Simple makefile added and some regex expressions

--- lab4-prelab/lukas.cpp
+++ lab4-prelab/lukas.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <regex>
 
 int main(int argc, char** argv) {
 	if(argc != 3)
@@ -7,8 +9,22 @@ int main(int argc, char** argv) {
 
 	std::ifstream input_file (argv[1]);
 
+	std::regex reg ("\w+\s");
+	std::string line;
+	while( std::getline(input_file, line) ) {
+		std::cout << line << std::endl;
+		if(std::regex_match(line, reg))
+			std::cout << "Line is matched by the regex" << std::endl;
+	}
+
 	std::ofstream output_file (argv[2]);
 
+
+	if( output_file.is_open() ) {
+		output_file << "This is a line\n";
+		output_file << "This is another line\n";
+	}
+		
 	input_file.close();
 	output_file.close();
 	return 0;
