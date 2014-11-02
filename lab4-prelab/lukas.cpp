e6d6c407da7ac75927b89bf0ed81ac69565e989a
Lukas Gemar <lukasgemar@gmail.com>
2014-11-02 02:44:22 -0500

    not adding non-register numbers

--- lab4-prelab/lukas.cpp
+++ lab4-prelab/lukas.cpp
@@ -1,25 +1,50 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <regex>
 #include <cstring>
+#include <map>
+#include <vector>
+#include <assert.h>
 
-typedef typename std::vector<std::pair<std::string, int>> label_table;
+typedef typename std::map<std::string, unsigned> string_map;
+typedef string_map label_table;
 typedef typename std::vector<std::string> string_array;
 typedef typename std::vector<string_array> instruction_table;
 static label_table labels;
 static instruction_table instructions;
-static int PC = 0;
+static unsigned PC = 0;
 
 bool is_label(const char* token);
 string_array parse_command(std::string line);
+bool has_only_whitespace(const std::string& str);
+void flush_commands(std::ofstream& output);
+
+static string_map Registers = {{"$0", 0}, {"$zero", 0}, {"$at", 1}, {"$v0", 2}, 
+							   {"$v1", 3}, {"$a0", 4}, {"$a1", 5}, 
+							   {"$a2", 6}, {"$a3", 7}, {"$t0", 8}, 
+							   {"$t1", 9}, {"$t2", 10}, {"$t3", 11}, 
+							   {"$t4", 12}, {"$t5", 13}, {"$t6", 14}, 
+							   {"$t7", 15}, {"$s0", 16}, {"$s1", 17},
+							   {"$s2", 18}, {"$s3", 19}, {"$s4", 20},
+							   {"$s5", 21}, {"$s6", 22}, {"$s7", 23},
+							   {"$t8", 24}, {"$t9", 25}, {"$k0", 26}, 
+							   {"$k1", 27}, {"$gp", 28}, {"$sp", 29}, 
+							   {"$fp", 30}, {"$ra", 31}}; 
+
+static string_map R_commands = {{"add", 32}, {"sub", 34}, {"and", 36}, 
+								{"or", 37}, {"xor", 38}, {"nor", 39}, 
+								{"slt", 42}, {"sll", 0}, {"sra", 3}, 
+								{"srl", 2}, {"jr", 8}};
+
+static string_map I_commands = {{"addi", 8}, {"andi", 12}, {"ori", 13}, 
+								{"xori", 14}, {"slti", 10}, {"beq", 4}, 
+								{"bne", 5}, {"lw", 35}, {"sw", 43}, {"nop", 0}};
+
+static string_map J_commands = {{"j", 2}, {"jal", 3}};
 
 int main(int argc, char** argv) {
 	if(argc != 3)
 		std::cout << "USAGE assemble input_file output_file" << std::endl;
-
-	// Store the names and locations of the labels
-	std::set<std::pair<std::string, int>> labels;
 
 	std::ifstream input_file (argv[1]);
 	std::ofstream output_file (argv[2]);
@@ -27,9 +52,14 @@ int main(int argc, char** argv) {
 	std::string line;
 	while( std::getline(input_file, line) ) {
 		string_array command;
-
 		command = parse_command(line);
+		if(!command.empty())
+			instructions.push_back(command);
 	}
+	std::cout << "There are " << instructions.size() << " instructions" << std::endl;
+	std::cout << "There are " << labels.size() << " labels" << std::endl;
+	std::cout << "Program counter is at " << std::hex << PC << std::endl;
+	flush_commands(output_file);
 	input_file.close();
 	output_file.close();
 	return 0;
@@ -41,30 +71,101 @@ bool is_label(const char* token) {
 	return (str.back() == ':');
 }
 
-label_table find_labels(std::ifstream input_file) {
+bool has_only_whitespace(const std::string& str) {
+	return str.find_first_not_of (" \t\n") == std::string::npos;
 }
 
 /* Returns the integer command represented by the line */
 string_array parse_command(std::string line) {
+	// Make a new command array
+	string_array command_array;
+
 	// Convert the std::string line to a char* 
 	char* cstr = new char[line.length() + 1];
 	strcpy(cstr, line.c_str());
 
-	// Make a new command array
-	string_array command_array;
-
 	// Parse the input
 	char* pch = strtok(cstr, " \t,");
+
+	// Make sure that this line is not only whitespace or is comment
+	if( has_only_whitespace(line) || *pch == '#' )
+		return command_array; // return an empty command array
+
 	while( pch != NULL && *pch != '#') {
 		std::cout << pch << std::endl;
 		if( is_label( pch ) ) {
+			std::cout << "Adding a new label" << std::endl;
 			const std::string s (pch);
-			labels.push_back(std::make_pair(std::string(pch), PC));
-			PC += 4;
+			string_map m;
+			m[s] = PC;
+			std::cout << "Size of labels " << labels.size() << std::endl;
 		}
-		else
-			(command_array).push_back(std::string(pch));
+		else {
+			command_array.push_back(std::string(pch));
+		}
 		pch = strtok(NULL, " \t,");
 	}
+	PC += 4;
 	return command_array;
+}
+
+unsigned lookup(std::string s) {
+	if( Registers.find(s) != Registers.end() ) {
+		return Registers[s];
+	}
+	else if ( labels.find(s) != labels.end() ) {
+		return labels[s];
+	}
+	else 
+		std::cout << "Unknown string: " << s << std::endl;
+	return 0;
+}
+
+unsigned lookup(unsigned u) {return u;};
+
+bool is_shift(std::string s) {
+	return (s == "sll" || s == "sra" || s == "srl" );
+}
+
+void flush_commands(std::ofstream& output) {
+	std::cout << "I am flushing commands" << std::endl;
+	for(auto it = instructions.begin(); it != instructions.end(); ++it) {
+		string_map::iterator pos;
+		unsigned result = 0;
+		if( (pos = R_commands.find((*it)[0])) != R_commands.end() ) {
+			assert( (*it).size() == 4 );
+			std::string the_command = (*it)[0];
+			result |= (*pos).second << 26;
+			if( is_shift( the_command) ) {
+				result |= lookup((*it)[2]) << 6;
+			}
+			else
+				result |= lookup((*it)[2]) << 21;
+			result |= lookup((*it)[1]) << 11;
+			result |= lookup((*it)[3]) << 16;
+			result |= 32;
+
+		}
+		else if( (pos = I_commands.find((*it)[0])) != I_commands.end() ) {
+			result |= (*pos).second << 26;
+			std::string the_command = (*it)[0];
+			result |= lookup((*it)[1]) << 21;
+			result |= lookup((*it)[2]) << 16;
+			int address = (int) lookup((*it)[3]);
+			if( address < 0 ) {
+				result |= 1 << 16;
+				result |= address;
+			}
+			else
+				result |= address;
+		}
+		else if( (pos = J_commands.find((*it)[0])) != J_commands.end() ) {
+			result |= (*pos).second << 26;
+			std::string the_command = (*it)[0];
+		}
+		else {
+			output << "There was an error in the assembly" << std::endl;
+		}
+		std::cout << std::hex << result << std::endl;
+	}
 }
