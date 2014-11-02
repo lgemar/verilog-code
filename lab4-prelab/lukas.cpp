fdb99664bc0941b1ece24ae8e2a53bafbd9f0913
Lukas Gemar <lukasgemar@gmail.com>
2014-11-01 20:34:43 -0400

    Correct parsing lines and labels

--- lab4-prelab/lukas.cpp
+++ lab4-prelab/lukas.cpp
@@ -2,19 +2,35 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <cstring>
 
 int main(int argc, char** argv) {
 	if(argc != 3)
 		std::cout << "USAGE assemble input_file output_file" << std::endl;
 
+	// Store the names and locations of the labels
+	std::set<std::pair<std::string, int>> labels;
+
 	std::ifstream input_file (argv[1]);
 
-	std::regex reg ("\w+\s");
 	std::string line;
 	while( std::getline(input_file, line) ) {
 		std::cout << line << std::endl;
-		if(std::regex_match(line, reg))
-			std::cout << "Line is matched by the regex" << std::endl;
+		// Convert the std::string line to a char* 
+		char* cstr = new char[line.length() + 1];
+		strcpy(cstr, line.c_str());
+
+		// Parse the input
+		char* pch = strtok(cstr, " \t,");
+		while( pch != NULL && *pch != '#') {
+			std::cout << pch << std::endl;
+			std::string str (pch);
+			if( str.back() == ':' )
+				std::cout << "This is a label" << std::endl;
+			pch = strtok(NULL, " \t,");
+		}
+
+		std::cout << "End of command" << std::endl;
 	}
 
 	std::ofstream output_file (argv[2]);
@@ -29,3 +45,34 @@ int main(int argc, char** argv) {
 	output_file.close();
 	return 0;
 }
+
+/**
+std::vector<base_class> table (add, sub, ... );
+
+struct base_class {
+	virtual int operator()(int a, int b) = 0;
+};
+
+struct add : public base_class {
+	int operator()(int a, int b) {
+		return a + b;
+	}
+};
+
+int c = add(first, second);
+
+struct sub {
+	std::string s;
+	int operator()(int a, int b) {
+		return a - b;
+	}
+}
+
+int d = sub(first, second);
+
+if( str == "add" )
+	add(str);
+else if (str == "sub") 
+	sub(str);
+8?
+*/
