8b397d36ae717161f7584b25bb3202a35abe0ff1
Lukas Gemar <lukasgemar@gmail.com>
2014-11-01 22:26:46 -0400

    Parsing all commands

--- lab4-prelab/lukas.cpp
+++ lab4-prelab/lukas.cpp
@@ -4,6 +4,16 @@
 #include <regex>
 #include <cstring>
 
+typedef typename std::vector<std::pair<std::string, int>> label_table;
+typedef typename std::vector<std::string> string_array;
+typedef typename std::vector<string_array> instruction_table;
+static label_table labels;
+static instruction_table instructions;
+static int PC = 0;
+
+bool is_label(const char* token);
+string_array parse_command(std::string line);
+
 int main(int argc, char** argv) {
 	if(argc != 3)
 		std::cout << "USAGE assemble input_file output_file" << std::endl;
@@ -12,67 +22,49 @@ int main(int argc, char** argv) {
 	std::set<std::pair<std::string, int>> labels;
 
 	std::ifstream input_file (argv[1]);
+	std::ofstream output_file (argv[2]);
 
 	std::string line;
 	while( std::getline(input_file, line) ) {
-		std::cout << line << std::endl;
-		// Convert the std::string line to a char* 
-		char* cstr = new char[line.length() + 1];
-		strcpy(cstr, line.c_str());
+		string_array command;
 
-		// Parse the input
-		char* pch = strtok(cstr, " \t,");
-		while( pch != NULL && *pch != '#') {
-			std::cout << pch << std::endl;
-			std::string str (pch);
-			if( str.back() == ':' )
-				std::cout << "This is a label" << std::endl;
-			pch = strtok(NULL, " \t,");
-		}
-
-		std::cout << "End of command" << std::endl;
+		command = parse_command(line);
 	}
-
-	std::ofstream output_file (argv[2]);
-
-
-	if( output_file.is_open() ) {
-		output_file << "This is a line\n";
-		output_file << "This is another line\n";
-	}
-		
 	input_file.close();
 	output_file.close();
 	return 0;
 }
 
-/**
-std::vector<base_class> table (add, sub, ... );
-
-struct base_class {
-	virtual int operator()(int a, int b) = 0;
-};
-
-struct add : public base_class {
-	int operator()(int a, int b) {
-		return a + b;
-	}
-};
-
-int c = add(first, second);
-
-struct sub {
-	std::string s;
-	int operator()(int a, int b) {
-		return a - b;
-	}
+/* Returns true if the command line is labelled */
+bool is_label(const char* token) {
+	std::string str (token);
+	return (str.back() == ':');
 }
 
-int d = sub(first, second);
+label_table find_labels(std::ifstream input_file) {
+}
 
-if( str == "add" )
-	add(str);
-else if (str == "sub") 
-	sub(str);
-8?
-*/
+/* Returns the integer command represented by the line */
+string_array parse_command(std::string line) {
+	// Convert the std::string line to a char* 
+	char* cstr = new char[line.length() + 1];
+	strcpy(cstr, line.c_str());
+
+	// Make a new command array
+	string_array command_array;
+
+	// Parse the input
+	char* pch = strtok(cstr, " \t,");
+	while( pch != NULL && *pch != '#') {
+		std::cout << pch << std::endl;
+		if( is_label( pch ) ) {
+			const std::string s (pch);
+			labels.push_back(std::make_pair(std::string(pch), PC));
+			PC += 4;
+		}
+		else
+			(command_array).push_back(std::string(pch));
+		pch = strtok(NULL, " \t,");
+	}
+	return command_array;
+}
