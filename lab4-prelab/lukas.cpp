697889fd4e776987fcb8655f308061634f4a601a
Lukas Gemar <lukasgemar@gmail.com>
2014-11-05 14:40:29 -0500

    Assembler updated after creation of functional dispatch in the flush_commands function

--- lab4-prelab/lukas.cpp
+++ lab4-prelab/lukas.cpp
@@ -5,6 +5,11 @@
 #include <map>
 #include <vector>
 #include <assert.h>
+#define JMASK 0x03ffffff
+#define TEXT_START 0x400000
+
+enum { SHIFT_OP = 26, SHIFT_RS = 21, SHIFT_RT = 16, 
+       SHIFT_RD = 11, SHIFT_SHMT = 6, SHIFT_OFFSET = 16 };
 
 typedef typename std::map<std::string, unsigned> string_map;
 typedef typename std::vector<std::string> string_array;
@@ -18,28 +23,23 @@ string_array parse_command(std::string line);
 bool has_only_whitespace(const std::string& str);
 void flush_commands(std::ofstream& output);
 
-static string_map Registers = {{"$0", 0}, {"$zero", 0}, {"$at", 1}, {"$v0", 2}, 
-							   {"$v1", 3}, {"$a0", 4}, {"$a1", 5}, 
-							   {"$a2", 6}, {"$a3", 7}, {"$t0", 8}, 
-							   {"$t1", 9}, {"$t2", 10}, {"$t3", 11}, 
-							   {"$t4", 12}, {"$t5", 13}, {"$t6", 14}, 
-							   {"$t7", 15}, {"$s0", 16}, {"$s1", 17},
-							   {"$s2", 18}, {"$s3", 19}, {"$s4", 20},
-							   {"$s5", 21}, {"$s6", 22}, {"$s7", 23},
-							   {"$t8", 24}, {"$t9", 25}, {"$k0", 26}, 
-							   {"$k1", 27}, {"$gp", 28}, {"$sp", 29}, 
-							   {"$fp", 30}, {"$ra", 31}}; 
-
-static string_map R_commands = {{"add", 32}, {"sub", 34}, {"and", 36}, 
-								{"or", 37}, {"xor", 38}, {"nor", 39}, 
-								{"slt", 42}, {"sll", 0}, {"sra", 3}, 
-								{"srl", 2}};
-
-static string_map I_commands = {{"addi", 8}, {"andi", 12}, {"ori", 13}, 
-								{"xori", 14}, {"slti", 10}, {"beq", 4}, 
-								{"bne", 5}, {"lw", 35}, {"sw", 43}, {"nop", 0}};
-
-static string_map J_commands = {{"j", 2}, {"jal", 3}, {"jr", 8}};
+static string_map Str2Reg = {
+			{"$0", 0}, {"$zero", 0}, {"$at", 1}, {"$v0", 2}, {"$v1", 3}, 
+			{"$a0", 4}, {"$a1", 5}, {"$a2", 6}, {"$a3", 7}, {"$t0", 8}, 
+			{"$t1", 9}, {"$t2", 10}, {"$t3", 11}, {"$t4", 12}, {"$t5", 13}, 
+			{"$t6", 14}, {"$t7", 15}, {"$s0", 16}, {"$s1", 17}, {"$s2", 18}, 
+			{"$s3", 19}, {"$s4", 20}, {"$s5", 21}, {"$s6", 22}, {"$s7", 23}, 
+			{"$t8", 24}, {"$t9", 25}, {"$k0", 26}, {"$k1", 27}, {"$gp", 28}, 
+			{"$sp", 29}, {"$fp", 30}, {"$ra", 31}}; 
+static string_map Rtype = {
+			{"add", 32}, {"sub", 34}, {"and", 36}, {"or", 37}, {"xor", 38}, 
+			{"nor", 39}, {"slt", 42},};
+static string_map Shifttype =  { {"sll", 0}, {"sra", 3}, {"srl", 2} };
+static string_map Itype = {{"addi", 8}, {"andi", 12}, {"ori", 13}, 
+								{"xori", 14}, {"slti", 10},{"nop", 0}};
+static string_map Jtype = {{"j", 2}, {"jal", 3}, {"jr", 8}};
+static string_map MemoryOps = { {"lw", 35}, {"sw", 43} };
+static string_map Branchtype = { {"beq", 4}, {"bne", 5} };
 
 int main(int argc, char** argv) {
 	if(argc != 3)
@@ -110,66 +110,122 @@ string_array parse_command(std::string line) {
 
 unsigned lookup(std::string s) {
 	std::cout << "looking up " << s << std::endl;
-	if( Registers.find(s) != Registers.end() ) {
-		return Registers[s];
+	if( Str2Reg.find(s) != Str2Reg.end() ) {
+		return Str2Reg[s];
 	}
 	else if ( labels.find(s) != labels.end() ) {
 		return labels[s];
 	}
 	else {
-		unsigned ret = std::stoi(s);
-		ret |= 1 << 15;
+		int ret = 0;
+		ret = std::stoi(s);
+		if( ret < 0 )
+			ret |= 1 << 15;
 		return ret;
 	}
 }
 
-unsigned lookup(unsigned u) {return u;};
+unsigned build_r(string_array s) {
+	unsigned result = 0;
+	std::string command = s[0];
+	result |= Rtype[command] << SHIFT_OP;
+	unsigned rd = lookup(s[1]);
+	unsigned rs = lookup(s[2]);
+	unsigned rt = lookup(s[3]);
+	return result | (rs << SHIFT_RS) | (rt << SHIFT_RT) | (rd << SHIFT_RD);
+}
 
-bool is_shift(std::string s) {
-	return (s == "sll" || s == "sra" || s == "srl" );
+unsigned build_i(string_array s) {
+	unsigned result = 0;
+	std::string command = s[0];
+	result |= Itype[command] << SHIFT_OP;
+	result |= lookup(s[1]) << SHIFT_RT;
+	result |= lookup(s[2]) << SHIFT_RS;
+	result |= (int) lookup(s[3]);
+	return result;
+}
+
+unsigned build_shift(string_array s) {
+	unsigned result = 0;
+	std::string command = s[0];
+	result |= Shifttype[command] << SHIFT_OP;
+	unsigned rd = lookup(s[1]);
+	unsigned rt = lookup(s[2]);
+	unsigned shamt = lookup(s[3]);
+    return result | (rd << SHIFT_RD) | (rt << SHIFT_RT) | (shamt << SHIFT_SHMT);
+}
+unsigned build_branch(string_array s) {
+	unsigned result = 0;
+	std::string command = s[0];
+	result |= Branchtype[command] << SHIFT_OP;
+	unsigned rs = lookup(s[1]);
+	unsigned rt = lookup(s[2]);
+	unsigned label_code = lookup(s[3]);
+	unsigned offset = (label_code - PC) >> 2;
+	return result | (rs << SHIFT_RS) | (rt << SHIFT_RT) | offset;
+}
+unsigned build_memoryop(string_array s) {
+	unsigned result = 0;
+	std::string command = s[0];
+	result |= MemoryOps[command] << SHIFT_OP;
+
+	unsigned rt = lookup(s[1]);
+	std::string memory_code = s[2];
+
+	// Convert the std::string line to a char* 
+	char* cstr = new char[memory_code.length() + 1];
+	strcpy(cstr, memory_code.c_str());
+
+	// Parse the input
+	char* pch = strtok(cstr, "()");
+	unsigned imm = lookup(std::string(pch));
+	
+	// Check for error
+	if( pch == NULL )
+		assert(false);
+
+	// Parse the second part of input: the register
+	pch = strtok(NULL, "()");
+	unsigned rs = lookup(std::string(pch));
+
+	// Check for error
+	if( pch == NULL )
+		assert(false);
+	
+	return result | (rs << SHIFT_RS) | (rt << SHIFT_RT) | imm;
+}
+
+unsigned build_jump(string_array s) {
+	unsigned result = 0;
+	std::string command = s[0];
+	result |=  Jtype[command] << SHIFT_OP;
+	unsigned target = ((TEXT_START + lookup(s[1])) >> 2) & JMASK;
+	return result | target;
 }
 
 void flush_commands(std::ofstream& output) {
 	std::cout << "I am flushing commands" << std::endl;
 	std::cout <<  "There are " << instructions.size() << " instructions" << std::endl;
 	for(auto it = instructions.begin(); it != instructions.end(); ++it) {
-		string_map::iterator pos;
 		unsigned result = 0;
-		std::string the_command = (*it)[0];
-		if( the_command == "nop" )
+		std::string command = (*it)[0];
+		if( command == "nop" ) {
 			continue;
-		else if( (pos = R_commands.find(the_command)) != R_commands.end() ) {
-			assert( (*it).size() == 4 );
-			std::string the_command = (*it)[0];
-			result |= (*pos).second << 26;
-			if( is_shift( the_command) ) {
-				result |= lookup((*it)[2]) << 6;
-			}
-			else
-				result |= lookup((*it)[2]) << 21;
-			result |= lookup((*it)[1]) << 11;
-			result |= lookup((*it)[3]) << 16;
-			result |= 32;
-
 		}
-		else if( (pos = I_commands.find(the_command)) != I_commands.end() ) {
-			result |= (*pos).second << 26;
-			if( the_command == "lw" || the_command == "sw") {
-				;// do nothing
-			}
-			else {
-				result |= lookup((*it)[1]) << 21;
-				result |= lookup((*it)[2]) << 16;
-				result = (int) lookup((*it)[3]);
-			}
-		}
-		else if( (pos = J_commands.find(the_command)) != J_commands.end() ) {
-			result |= (*pos).second << 26;
-			std::string the_command = (*it)[0];
-		}
-		else {
+		else if( Rtype.find(command) != Rtype.end() )
+			result = build_r( *it );
+		else if( Shifttype.find(command) != Shifttype.end() )
+			result = build_shift( *it );
+		else if( Itype.find(command) != Itype.end() )
+			result = build_i( *it );
+		else if( MemoryOps.find(command) != MemoryOps.end() )
+			result = build_memoryop( *it );
+		else if ( Branchtype.find(command) != Branchtype.end() )
+			result = build_branch( *it );
+		else if( Jtype.find(command) != Jtype.end() )
+			result = build_jump( *it );
+		else
 			output << "There was an error in the assembly" << std::endl;
-		}
 		std::cout << "A new instruction: ";
 		std::cout << std::hex << result << std::endl;
 	}
