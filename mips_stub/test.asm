db853799618b74e7cec514ea1136073747f8d263
Lukas Gemar <lukasgemar@gmail.com>
2014-11-20 17:53:38 -0500

    most R-type instructions are working and some i-type instructions

--- mips_stub/test.asm
+++ mips_stub/test.asm
@@ -1,32 +1,33 @@
 # Test load words and store words
-main:   lw	$t0, 0($zero)
-	sw	$t0, 80($zero)
+main: addi $sp, $zero, 200
+	lw	$t0, 0($zero)
+	sw	$t0, 0($sp)
 	lw	$t1, 4($zero)
-	sw	$t1, 84($zero)
+	sw	$t1, 4($sp)
 	lw	$t2, 8($zero)
-	sw	$t2, 88($zero)
+	sw	$t2, 8($sp)
 
 	# Test R-type instructions
 	addi $t3, $zero, 3
 	addi $t4, $zero, 7
 	and $s0, $t3, $t4
-	sw $s0, 92($zero) # out = 3
+	sw $s0, 12($sp) # out = 3
 	or $s1, $t3, $t4
-	sw $s1, 96($zero) # out = 7
+	sw $s1, 16($sp) # out = 7
 	add $s2, $t3, $t4
-	sw $s2, 100($zero) # out = 10
+	sw $s2, 20($sp) # out = 10
 	beq $s0, $s0, dest2
 
 	# I-type instructions
 
 dest1: nor $s5, $t3, $t4
-   sw $s5, 112($zero) # out = something crazy
+   sw $s5, 32($sp) # out = something crazy
    beq $s5, $s5, finish # always true
 
 dest2: sub $s3, $t4, $t3
-	sw $s3, 104($zero) # out = 4
+	sw $s3, 24($sp) # out = 4
 	xor $s4, $t3, $t4
-	sw $s4, 108($zero) # out = 4
+	sw $s4, 28($sp) # out = 4
 	beq $s4, $s4, dest1
 
 finish: add $zero, $zero, $zero #dummy instruction
