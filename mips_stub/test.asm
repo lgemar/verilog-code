ee7252b4c735424f24d90ce4b1dc753e46542066
Lukas Gemar <lukasgemar@gmail.com>
2014-11-20 16:21:54 -0500

    memory ops, r-type, and branch instructions working

--- mips_stub/test.asm
+++ mips_stub/test.asm
@@ -7,27 +7,21 @@ main:   lw	$t0, 0($zero)
 	sw	$t2, 88($zero)
 
 	# Test R-type instructions
-
 	and $s0, $t0, $zero
-	sw $s0, 92($zero) # out = 0x0
-
 	or $s1, $t0, $zero
 	sw $s1, 96($zero) # out = $t0
-
 	add $s2, $t0, $zero
 	sw $s2, 100($zero) # out = $t0
+	beq $s1, $s2, dest2
 
-	sub $s3, $t0, $t0
+dest1: nor $s5, $t0, $t0
+	sw $s5, 112($zero) # out = !$t0
+	beq $s1, $s2, finish # always true
+
+dest2: sub $s3, $t0, $t0
 	sw $s3, 104($zero) # out = 0
-
 	xor $s4, $t0, $t0
 	sw $s4, 108($zero) # out = 0
+	beq $s3, $s4, dest1
 
-	nor $s5, $t0, $t0
-	sw $s5, 112($zero) # out = !$t0
-
-
-
-
-
-	
+finish: add $zero, $zero, $zero #dummy instruction
