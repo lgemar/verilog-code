51a4c2ec59607bc6bf6b4078dd1fac1815cd5762
Lukas Gemar <lukasgemar@gmail.com>
2014-11-21 13:12:06 -0500

    i-type and jump commands are working

--- mips_stub/test.asm
+++ mips_stub/test.asm
@@ -18,27 +18,35 @@ main: addi $sp, $zero, 200
 	sw $s2, 20($sp) # out = 10
 
 	# I-type instructions
-	#lui $s7, 291
 	ori $s7, $s2, 5
-	sw $s7, 24($sp)
+	sw $s7, 24($sp) # out = f
 
 	xori $s7, $s2, 5
-	sw $s7, 28($sp)
+	sw $s7, 28($sp) # out = f
 
 	andi $s7, $s2, 5
-	sw $s7, 32($sp)
+	sw $s7, 32($sp) # out = 0
+
+	slti $s7, $s2, 5
+	sw $s7, 36($sp) # out = 0
+
+	slti $s7, $s2, 15
+	sw $s7, 40($sp) # out = 1
+
+	# Jump to the finish
+	j dest1
 
 	# Test branches
 	beq $s0, $s0, dest2
 
 dest1: nor $s5, $t3, $t4
-   sw $s5, 44($sp) # out = something crazy
+   sw $s5, 52($sp) # out = something crazy
    beq $s5, $s5, finish # always true
 
 dest2: sub $s3, $t4, $t3
-	sw $s3, 36($sp) # out = 4
+	sw $s3, 44($sp) # out = 4
 	xor $s4, $t3, $t4
-	sw $s4, 40($sp) # out = 4
+	sw $s4, 48($sp) # out = 4
 	beq $s4, $s4, dest1
 
 finish: add $zero, $zero, $zero #dummy instruction
